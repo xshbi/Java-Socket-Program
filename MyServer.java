a30b91a1720cc0d52ac8af7b30dda27d23f6b5b1
Akashvi <109546133+xshbi@users.noreply.github.com>
2024-06-20 21:14:02 +0530

    Add files via upload
    This is output

---
+++ MyServer.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.net.*;
+
+public class MyServer {
+    public static void main(String[] args) throws Exception {
+        ServerSocket ss = new ServerSocket(3333);
+        System.out.println("Server started, waiting for client...");
+
+        Socket s = ss.accept();
+        System.out.println("Client connected.");
+
+        DataInputStream din = new DataInputStream(s.getInputStream());
+        DataOutputStream dout = new DataOutputStream(s.getOutputStream());
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str = "", str2 = "";
+        while (!str.equals("stop")) {
+            str = din.readUTF();
+            System.out.println("Client says: " + str);
+            str2 = br.readLine();
+            dout.writeUTF(str2);
+            dout.flush();
+        }
+        din.close();
+        s.close();
+        ss.close();
+    }
+}
